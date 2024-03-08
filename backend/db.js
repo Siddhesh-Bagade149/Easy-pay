d64d1e8fa9d1172322f03003d5b3257de8d5e204
Siddhesh Bagade <siddheshbagade1618@gmail.com>
2024-03-08 18:11:02 +0530

    added a db.js

---
+++ backend/db.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 40
+    },
+    password:{
+        typeof:String,
+        required:true,
+        minLength:6
+    },
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    }
+});
+
+const userModel=mongoose.model('User',userSchema);
+
+module.exports={
+    userModel
+}
