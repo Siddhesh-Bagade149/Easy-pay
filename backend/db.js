19bc1b2207f3dae734e54a71a281e42f60866074
Siddhesh Bagade <siddheshbagade1618@gmail.com>
2024-03-08 19:05:13 +0530

    added a router

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
+// Connect MongoDB at default port 27017.
+mongoose.connect(process.env.MONGO_URI, {
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
+
 const userSchema=new mongoose.Schema({
     username: {
         type: String,
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
 const express = require("express");
+const rootRouter = require('./routes/index')
+const app = express();
 
-
+app.use("/api/v1",rootRouter)
