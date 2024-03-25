34bed03a05e52fd66f2a7079bb9f3769f8802fdf
Siddhesh Bagade <siddheshbagade1618@gmail.com>
2024-03-25 23:30:45 +0530

    changed default route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={(token)?<Dashboard/>:<Signin/> } />
+          {/* <Route path="/" element={(token)?<Dashboard/>:<Signin/> } /> */}
+          <Route path="/" element={<Signin/> } />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
