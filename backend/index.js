5eaee3af99d0592c4af156d8c9d9ba7d43191d8f
Siddhesh Bagade <siddheshbagade1618@gmail.com>
2024-05-21 15:52:54 +0530

    adding backend to render

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const rootRouter = require('./routes/index')
 const app = express();
 const cors= require('cors')
 // const bodyParser = require('body-parser'); 
+const port=process.env.PORT || 3000
 
 app.use(cors({
     origin:["https://easy-pay-pi.vercel.app/"],
@@ -13,6 +14,6 @@ app.use(express.json()) // no need for bodyparse.json() anymore
 
 app.use("/api/v1",rootRouter)
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('server running at 3000 ');
 })
