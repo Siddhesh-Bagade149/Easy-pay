f3cfc235fa2d90d8505dbd1c672b052afec5e772
Siddhesh Bagade <siddheshbagade1618@gmail.com>
2024-05-21 15:26:45 +0530

    starting ddeployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
