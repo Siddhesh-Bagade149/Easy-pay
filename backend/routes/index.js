4b800d6a8d8ce94efd2a5ba48c417b5b5eeaaf49
Siddhesh Bagade <105978276+Siddhesh-Bagade149@users.noreply.github.com>
2024-05-22 09:18:59 +0530

    added / route for testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.use('/user',userRouter)
 router.use('/account',accountRouter);
+router.get('/', (req, res) => {
+    res.send('GET request to the /')
+  })
 
 module.exports = router;
