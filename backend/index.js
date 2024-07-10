9645b0789fcf60301eeea8a784c9de9893b2f071
Siddhesh Bagade <siddheshbagade1618@gmail.com>
2024-07-10 21:08:40 +0530

    changed cors to *

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000
 
 app.use(cors({
-    origin: ["https://easy-pay-pi.vercel.app/"],
+    origin: ["*"],
     methods: ["POST", "GET", "PUT"],
     credentials: true
 }))
